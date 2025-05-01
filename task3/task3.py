40902b55085ea12b7f16a62f4b4caadeca9c6820
PechenenkoYara <pechenenko.pn@ucu.edu.ua>
2025-05-01 22:00:17 +0300

    third task

---
+++ task3/task3.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def deleteNode(self, root, key):
+        if not root:
+            return None
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            # Node with only one child or no child
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+
+            min_larger_node = self.get_min(root.right)
+            root.val = min_larger_node.val
+            root.right = self.deleteNode(root.right, min_larger_node.val)
+
+        return root
+
+    def get_min(self, node):
+        while node.left:
+            node = node.left
+        return node
